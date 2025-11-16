171f57ea3097b36de10b2b3f4aa0dc065b84f0c5
Manuel Christian <mytrojan001@gmail.com>
2025-11-16 16:25:01 +0700

    minor landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
 const LandingPage = () => {
   return (
-    <main className = "w-full h-screen">
-      <h1 className = "text-4xl">Cari umkm</h1>
+    <main className = "w-full h-screen p-8 flex flex-col justify-center gap-2">
+      <h1 className = "text-4xl font-medium font-semibold">Jelajahi UMKM di sekitarmu</h1>
+      <span className = "text-lg text-gray-500">Bersama Sekitar, jelajahi UMKM yang ada di sekitarmu dengan lebih cepat, mudah, dan seru!</span>
+      <Link href = "/dashboard">
+        <Button className = "text-lg px-8 mt-4">
+          Mulai
+        </Button>
+      </Link>
     </main>
   )
 }
